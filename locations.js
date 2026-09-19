367f607226e1d98e935b9257dd28c38025ce2774
SANJI <hoangtuan17012015@gmail.com>
2026-09-19 20:35:52 +0700

    test-2

---
+++ locations.js
@@ -0,0 +1,22 @@
+const locationsData = [
+  {
+    id: 1,
+    name: "200/400 Vietnam VN",
+    mapLink: "https://maps.app.goo.gl/UDKUYE9ishwYFSi77",
+    image: "images/quan-1.jpg",
+    city: "Riyadh",
+    info: "",
+    lat: 10.8862207,
+    lng: 106.8078986,
+  },
+  {
+    id: 2,
+    name: "Địa điểm mẫu 2",
+    mapLink: "https://maps.google.com/?q=10.776889,106.700806",
+    image: "images/quan-2.jpg",
+    city: "TP.HCM",
+    info: "Gần trung tâm",
+    lat: 10.776889,
+    lng: 106.700806,
+  }
+];
